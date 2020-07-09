2b3e2e8e52c50bd350d0f0054d6a2ee7e923ae3f
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-07-08 21:38:27 -0400

    remove delay on highlight bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -136,7 +136,7 @@ class HomePage extends React.Component {
           </div>
         </Section>
 
-        <Section id="highlight" className="bg-highlight pt-10 pb-10" data-aos="fade-up" data-aos-delay="500">
+        <Section id="highlight" className="bg-highlight pt-10 pb-10" data-aos="fade-up">
           <Grid container justify="center">
             <Grid item>
               <EditableLink content={content["apply-now-text"]} onSave={this.onSave('apply-now-text')} />
